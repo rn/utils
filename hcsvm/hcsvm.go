e54497186316f1b6817ee9370032525911482ac6
Rolf Neugebauer <rolf.neugebauer@gmail.com>
2017-10-30 16:08:04 +0000

    hscvm: Add some command line arguments
    Also use a UUID v4 as the default VM name
    
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@gmail.com>

--- hcsvm/hcsvm.go
+++ hcsvm/hcsvm.go
@@ -1,24 +1,41 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/Microsoft/hcsshim"
+	"github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	imgPath := flag.String("dir", "C:\\Program Files\\Linux Containers", "Directory with initrd.img and bootx64.efi")
+	name := flag.String("name", "", "Name of the VM (default a UUID v4)")
+	extraCmdLine := flag.String("cmdline", "", "Additional kernel command line arguments")
+
+	flag.Parse()
+
+	if *name == "" {
+		*name = uuid.NewV4().String()
+	}
+
+	cmdLine := "console=ttyS0"
+	if *extraCmdLine != "" {
+		cmdLine = cmdLine + " " + *extraCmdLine
+	}
 
 	logrus.SetLevel(logrus.DebugLevel)
 
 	runtimeCfg := &hcsshim.HvRuntime{
-		ImagePath:           "C:\\Program Files\\Linux Containers",
+		ImagePath:           *imgPath,
 		LinuxInitrdFile:     "initrd.img",
 		LinuxKernelFile:     "bootx64.efi",
-		LinuxBootParameters: "console=ttyS0",
+		LinuxBootParameters: cmdLine,
 	}
 
 	vmCfg := &hcsshim.ContainerConfig{
 		SystemType:                  "container",
-		Name:                        "FooBar",
+		Name:                        *name,
 		Owner:                       "Me",
 		HvPartition:                 true,
 		ContainerType:               "linux",
@@ -27,18 +44,19 @@ func main() {
 		HvRuntime: runtimeCfg,
 	}
 
-	logrus.Info("Create VM")
+	logrus.Info("Create VM: %s", *name)
 	vm, err := hcsshim.CreateContainer(vmCfg.Name, vmCfg)
 	if err != nil {
-		logrus.Fatal("CreateContainer(): %v\n", err)
+		logrus.Fatalf("CreateContainer(): %v\n", err)
 	}
+	// XXX The above hangs and times out after 240s
 
-	logrus.Info("Start VM")
-	if err = vm.Start(); err != nil {
-		logrus.Warnf("Start(): %v\n", err)
-		vm.Terminate()
-		return
-	}
+	// logrus.Info("Start VM")
+	// if err = vm.Start(); err != nil {
+	// 	logrus.Warnf("Start(): %v\n", err)
+	// 	vm.Terminate()
+	// 	return
+	// }
 
 	logrus.Info("Waiting to terminate")
 	vm.Wait()
